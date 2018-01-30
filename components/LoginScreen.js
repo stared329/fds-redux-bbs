ea30bd8cf6db5ae89699086bdfd8db55d022d1fb
stared329 <jamie.kim.0329@gmail.com>
2018-01-30 14:47:27 +0900

    로그인 화면

---
+++ components/LoginScreen.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {Button, Icon, Grid, Segment, Header} from 'semantic-ui-react';
+import styled from 'styled-components';
+
+const FullHeightGrid = styled(Grid)`
+  height: 100vh;
+`;
+
+export default class LoginScreen extends Component{
+  render() {
+    return (
+      <FullHeightGrid centered verticalAlign="middle" columns={3}>
+        <Grid.Column>
+          <Segment textAlign="center" padded="very">
+            <Header as="h1">로그인</Header>
+            <Button color='google plus' fluid>
+              <Icon name='google plus' /> 구글로 로그인
+            </Button>
+            <Button color='twitter' fluid>
+              <Icon name='twitter' /> 트위터로 로그인
+            </Button>
+          </Segment>
+        </Grid.Column>
+      </FullHeightGrid>
+    )
+  }
+}
--- stories/index.js
+++ stories/index.js
@@ -1,11 +1,2 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-
-storiesOf('Button', module)
-  .add('with text', () => (
-    <button onClick={action('clicked')}>Hello Button</button>
-  ))
-  .add('with some emoji', () => (
-    <button onClick={action('clicked')}>ddd</button>
-  ));
+ import './LoginScreen';
+import 'semantic-ui-css/semantic.min.css';
