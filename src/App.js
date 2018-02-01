c371966c6ca3df435beca1ca881701c02969dbec
stared329 <jamie.kim.0329@gmail.com>
2018-02-01 17:24:52 +0900

    redux 연결

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import thunk from 'redux-thunk';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import LoginScreenContainer from './containers/LoginScreenContainer';
+import ListPage from './containers/ListPage';
+import withAuth from './hocs/withAuth';
+import rootReducer from './ducks';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+const Home = withAuth(() => <Redirect to="/list" />);
 
 class App extends Component {
   render() {
     return (
-     <BrowserRouter>
-      <div>
-        <Route path="/login" component={LoginScreenContainer} />>
-      </div>
-     </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={Home} />
+            <Route path="/list" component={ListPage} />
+            <Route path="/login" component={LoginScreenContainer} />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
---
+++ src/ducks/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import articleList from './articleList';
+
+export default combineReducers({
+  articleList,
+});
